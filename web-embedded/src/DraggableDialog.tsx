a695bc64e53a52a26740fce8934283d6c9681d7b
nedphae <nedphae@gmail.com>
2022-07-04 23:00:06 +0800

    修复弹窗遮挡

--- web-embedded/src/DraggableDialog.tsx
+++ web-embedded/src/DraggableDialog.tsx
@@ -225,7 +225,7 @@ export default function DraggableDialog(accessParamProp: DraggableDialogProps) {
           </div>)}
         </div>
       </Button>
-      <Popper open={open} hidden={hidden} anchorEl={null} style={{ zIndex: 100000 }}>
+      <Popper open={open} hidden={hidden} anchorEl={null} style={{ zIndex: 100000, height: 0, }}>
         <DraggableOrNot handle="#draggable-dialog-title" cancel={'[class*="MuiDialogContent-root"],[class*="MuiButtonBase-root"]'} >
           {/* 使用 CSS-in-JS */}
           <DialogTitle style={{ cursor: 'move', width: '100%', height: '44px', position: 'absolute', padding: '0px 0px', zIndex: 100001 }} id="draggable-dialog-title">
